060b9baf59f067f72a1e9b4b043229abffa03f69
Gabriel <gaba93@yandex.ru>
2021-10-27 16:26:10 +0300

    input form layout and state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import data from './flights.json';
 function App() {
   const [flightsData, setFlightsData] = useState();
   const [radioInputValue, setRadioInputValue] = useState('ascending');
+  const [checkedState, setCheckedState] = useState([true, false, false, false]);
+  const [priceInputValue, setPriceInputValue] = useState({
+    from: 0,
+    till: 10000,
+  });
 
   console.log(data.result.flights[0]);
 
@@ -15,11 +20,27 @@ function App() {
 
   const handleRadioInputChange = (e) => setRadioInputValue(e.target.value);
 
+  const handleCheckboxInputChange = (e) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === Number(e.target.value) ? !item : item
+    );
+
+    setCheckedState(updatedCheckedState);
+  };
+
+  const handlePriceInputChange = (e) => {
+    setPriceInputValue({ ...priceInputValue, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="main-container">
       <Form
         radioInputValue={radioInputValue}
         handleRadioInputChange={handleRadioInputChange}
+        handleCheckboxInputChange={handleCheckboxInputChange}
+        checkedState={checkedState}
+        priceInputValue={priceInputValue}
+        handlePriceInputChange={handlePriceInputChange}
       />
       <Flights flightsData={flightsData} />
     </div>
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
-function Form({ radioInputValue, handleRadioInputChange }) {
+function Form({
+  radioInputValue,
+  handleRadioInputChange,
+  handleCheckboxInputChange,
+  checkedState,
+  priceInputValue,
+  handlePriceInputChange,
+}) {
   return (
     <div className="form-container">
       {/* radio buttons sort */}
@@ -46,8 +53,85 @@ function Form({ radioInputValue, handleRadioInputChange }) {
       {/* checkbox filters */}
       <div className="input-container">
         <p>Фильтровать</p>
+        <div className="input-group">
+          {' '}
+          <input
+            type="checkbox"
+            id="transfer"
+            value="0"
+            checked={checkedState[0]}
+            onChange={(e) => handleCheckboxInputChange(e)}
+          />
+          <label for="transfer"> - 1 пересадка</label>
+        </div>
+        <div className="input-group">
+          {' '}
+          <input
+            type="checkbox"
+            id="no_transfer"
+            value="1"
+            checked={checkedState[1]}
+            onChange={(e) => handleCheckboxInputChange(e)}
+          />
+          <label for="no_transfer"> - без пересадок</label>
+        </div>
       </div>
       {/* checkbox filters */}
+      {/* price filters */}
+      <div className="input-container">
+        {' '}
+        <p>Цена</p>
+        <div className="input-group price">
+          {' '}
+          <label for="price-from">От</label>
+          <input
+            type="number"
+            id="price-from"
+            name="from"
+            value={priceInputValue.from}
+            onChange={(e) => handlePriceInputChange(e)}
+          />
+        </div>
+        <div className="input-group price">
+          {' '}
+          <label for="price-till">До</label>
+          <input
+            type="number"
+            id="price-till"
+            name="till"
+            value={priceInputValue.till}
+            onChange={(e) => handlePriceInputChange(e)}
+          />
+        </div>
+      </div>
+      {/* price filters */}
+      {/* Airline filters */}
+      <div className="input-container">
+        <p>Авиакомпании</p>
+        <div className="input-group">
+          {' '}
+          <input
+            type="checkbox"
+            id="polish"
+            value="2"
+            onChange={(e) => handleCheckboxInputChange(e)}
+            checked={checkedState[2]}
+          />
+          <label for="polish"> - LOT Polish Airlines от 21049 р.</label>
+        </div>
+        <div className="input-group">
+          {' '}
+          <input
+            type="checkbox"
+            id="aeroflot"
+            value="3"
+            onChange={(e) => handleCheckboxInputChange(e)}
+            checked={checkedState[3]}
+          />
+          <label for="aeroflot"> - Аэрофлот - росс.... от 31733 р.</label>
+        </div>
+      </div>
+      {/* Airline filters */}
     </div>
   );
 }
